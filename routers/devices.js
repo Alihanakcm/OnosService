6fe76d9a82d4583948615b18875e4d905124fc4a
alihanakcm <alihanakcm@gmail.com>
2020-07-24 18:59:10 +0300

    feat: Device s route added

--- routers/devices.js
+++ routers/devices.js
@@ -1,1 +1,22 @@
+var express = require("express");
+var app = express();
+const superagent = require("superagent");
 
+require("dotenv").config();
+app.get("/devices", (req, res) => {
+  try {
+    res.send("grld");
+    superagent
+      .get("http://192.168.56.1:8181/onos/v1/devices")
+      .auth((usr = "onos"), (pass = "rocks"))
+      .end((err, res) => {
+        if (err) throw err;
+        console.log("hadi adadsd");
+        console.log(JSON.parse(res.text));
+      });
+    console.log("geldisds");
+  } catch (error) {
+    res.send(error);
+  }
+});
+var server = app.listen(5000);
